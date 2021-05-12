dcdcfd85586586018b6dcd968c08766b1721658d
Ivan_Khailo <jakom333@gmail.com>
2021-05-12 11:18:24 +0300

    Task1: data-bound-component

---
+++ Data-bound-component/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h4>Replace this line with your component</h4>
+  `,
+})
+export class AppComponent {
+}
