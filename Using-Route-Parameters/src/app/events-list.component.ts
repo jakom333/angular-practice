eafae4ad46212b3c867bf5a82b6d200c4f01ef39
Ivan_Khailo <jakom333@gmail.com>
2021-05-13 15:11:33 +0300

    Task15: Solution - Update the `event-details` component to use the parameter when calling the events service.

---
+++ Using-Route-Parameters/src/app/events-list.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {EventService} from './event.service';
+
+@Component({
+    selector: 'events-list',
+    templateUrl: 'events-list-component.html',
+    styles: [`
+        th, td {
+            font-size: 16px;
+            padding: 5px 10px;
+        }
+
+        td a {
+            color: orange
+        }
+    `]
+})
+export class EventsListComponent implements OnInit {
+    events: any[];
+
+    constructor(private eventService: EventService) {
+
+    }
+
+
+    ngOnInit(): void {
+        this.events = this.eventService.getEvents();
+    }
+
+
+}
