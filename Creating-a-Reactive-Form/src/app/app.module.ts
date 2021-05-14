dbaf922219b4d36d2c6b0a772968acc07296de36
Ivan_Khailo <jakom333@gmail.com>
2021-05-14 12:10:37 +0300

    Task23: Solution - Wired up the form using Reactive (model-based) forms so that the data entered can be saved as a new event.

--- Creating-a-Reactive-Form/src/app/app.module.ts
+++ Creating-a-Reactive-Form/src/app/app.module.ts
@@ -7,7 +7,7 @@ import {EventService} from './event.service';
 import {RouterModule} from '@angular/router';
 import {appRoutes} from './routes';
 import {CreateEventComponent} from './create-event.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -18,6 +18,7 @@ import {FormsModule} from '@angular/forms';
     imports: [
         BrowserModule,
         FormsModule,
+        ReactiveFormsModule,
         RouterModule.forRoot(appRoutes)
     ],
     providers: [EventService],
--- Creating-a-Reactive-Form/src/app/create-event.component.ts
+++ Creating-a-Reactive-Form/src/app/create-event.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
 import {EventService} from './event.service';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
     selector: 'create-event',
@@ -21,12 +22,37 @@ import {EventService} from './event.service';
     `]
 })
 export class CreateEventComponent implements OnInit {
+    createForm: FormGroup;
+    location: FormGroup;
+    name: FormControl = new FormControl();
+    date: FormControl = new FormControl();
+    time: FormControl = new FormControl();
+    address: FormControl = new FormControl();
+    city: FormControl = new FormControl();
+    country: FormControl = new FormControl();
 
     constructor(private eventService: EventService, private router: Router) {
 
     }
 
     ngOnInit(): void {
+        this.location = new FormGroup({
+            address: this.address,
+            city: this.city,
+            country: this.country
+        });
+
+        this.createForm = new FormGroup({
+            name: this.name,
+            date: this.date,
+            time: this.time,
+            location: this.location
+        });
+    }
+
+    save(event: any){
+        this.eventService.saveEvent(event);
+        this.router.navigate(['/events']);
     }
 
     cancel(): void {
