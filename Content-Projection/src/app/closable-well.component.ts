5ecfd36844beb698f7f7617d6624eb7eb3aeccb9
Ivan_Khailo <jakom333@gmail.com>
2021-05-14 17:12:30 +0300

    Task26: Content Projection.

---
+++ Content-Projection/src/app/closable-well.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'closable-well',
+    template: `
+        <button type="button" class="btn btn-primary">Toggle</button>
+    `
+})
+
+export class ClosableWellComponent {
+
+}
