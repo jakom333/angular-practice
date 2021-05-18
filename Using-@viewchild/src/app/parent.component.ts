de267af3de6419ccea15ffa56a7ff81f2d42768a
Ivan_Khailo <jakom333@gmail.com>
2021-05-18 10:41:56 +0300

    Task37: 1. add a ref to the correct div (#varname)         2. Use a @ViewChild to grab that div         3. in the reveal method, bind the innertext of the nativeElement to the greatest property.

---
+++ Using-@viewchild/src/app/parent.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'parent',
+    template: `
+
+        <h1>Using View Child</h1>
+        <div style="display:none">Star Wars</div>
+        <div style="display:none">Jaws</div>
+        <div style="display:none">Titanic</div>
+        <div style="display:none">Avatar</div>
+        <div style="display:none">The Odd Couple</div>
+        <div style="display:none">You've Got Mail</div>
+        <div>The greatest movie of all time: {{greatest}}</div>
+        <button class="btn btn-primary" (click)="reveal()">Click to Show</button>
+    `
+})
+export class ParentComponent implements OnInit {
+    greatest: string;
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    reveal () {
+        this.greatest = ;
+    }
+
+}
