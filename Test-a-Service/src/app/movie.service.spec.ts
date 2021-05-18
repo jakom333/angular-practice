7aed6243948857267d8c29a781fecfb80722f12f
Ivan_Khailo <jakom333@gmail.com>
2021-05-18 16:45:01 +0300

    Task41: Solution - created a it function that simply tests that true is true.

--- Test-a-Service/src/app/movie.service.spec.ts
+++ Test-a-Service/src/app/movie.service.spec.ts
@@ -4,10 +4,12 @@ describe('MovieService', () => {
     let movieService: MovieService;
 
     beforeEach(() => {
-
+        movieService = new MovieService('Rambo');
     });
 
     it('adding a cast member increases the cast member count by one', () => {
+        movieService.addCastMember('Sylveter', 'Rambo');
 
+        expect(movieService.castMembers.length).toBe(1);
     });
 });
