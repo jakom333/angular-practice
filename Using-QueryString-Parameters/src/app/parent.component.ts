b003d9b28dbd0c65f1d778f38b96434669b6ae99
Ivan_Khailo <jakom333@gmail.com>
2021-05-18 12:32:25 +0300

    Task40: 1. in the makeRequest method, create the correct URL string and pass it to the http.get method.

---
+++ Using-QueryString-Parameters/src/app/parent.component.ts
@@ -0,0 +1,36 @@
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+@Component({
+    selector: 'parent',
+    template: `
+        <h1>Using Querystring Parameters</h1>
+
+        <form>
+            City: <input type="text" name="city" [(ngModel)]="city"> <br> <br>
+            <button class="btn btn-primary" type="submit" (click)="makeRequest()">Get Weather</button>
+        </form>
+        <br> <br>
+
+        Tomorrow's Forecast: {{forecast}}
+    `
+})
+export class ParentComponent {
+    appid = "69bc9bb1bbaa496601358677ea57a017";
+    city;
+    forecast;
+
+    constructor(private http: HttpClient) {
+    }
+
+    makeRequest(): void {
+        this.http.get().pipe(map((response: any) => {
+                return response.weather[0].description;
+            })
+        )
+            .subscribe((forecast: any) => {
+                this.forecast = forecast;
+            });
+    }
+}
