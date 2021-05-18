c72bf5e298d0a76c33dfdc8201b55b4463fc6bbf
Ivan_Khailo <jakom333@gmail.com>
2021-05-18 12:06:13 +0300

    Task39: 1. update the .get() call to use the .pipe function and map operator to adjust the data so that the given subscribe call works.

---
+++ Messaging-Requested-Data-with-Map/src/app/parent.component.ts
@@ -0,0 +1,28 @@
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+@Component({
+    selector: 'parent',
+    template: `
+        <h1>Making a Request</h1>
+
+        <button class="btn btn-primary" (click)="makeRequest()">Send Request</button>
+
+        <div *ngFor="let movie of movieList">
+            {{movie.title}}
+        </div>
+    `
+})
+export class ParentComponent {
+    movieList: string[];
+
+    constructor(private http: HttpClient) {
+    }
+
+    makeRequest(): void {
+        this.http.get('http://swapi.dev/api/films/').subscribe((data: any) => {
+            this.movieList = data.results;
+        });
+    }
+}
